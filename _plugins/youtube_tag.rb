1372130c5bcbe973335c597321c13a4f44804b0d
msjo <me@msjo.kr>
2019-05-10 00:47:23 +0900

    Update youtube_tag.rb

--- _plugins/youtube_tag.rb
+++ _plugins/youtube_tag.rb
@@ -1,30 +1,28 @@
-module Jekyll
-  class YouTubeTag < Liquid::Tag
-    @videoid = nil
-    @width = ''
-    @height = ''
+class YouTube < Liquid::Tag
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
-    def initialize(tag_name, markup, tokens)
-      if markup =~ /(?:(?:https?:\/\/)?(?:www.youtube.com\/(?:embed\/|watch\?v=)|youtu.be\/)?(\S+)(?:\?rel=\d)?)(?:\s+(\d+)\s(\d+))?/i
-        @videoid = $1
-        @width = $2 || "640"
-        @height = $3 || "480"
-      end
-      super
-    end
+  def initialize(tagName, markup, tokens)
+    super
 
-    def render(context)
-      ouptut = super
-      if @videoid
-        intrinsic = ((@height.to_f / @width.to_f) * 100)
-        padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
-        video = "<style>.bt-video-container iframe,.bt-video-container object,.bt-video-container embed{position:absolute;top:0;left:0;width:100%;height:100%;margin-top:0}</style>\n"
-        video += %Q{<div class="bt-video-container" style="position:relative;padding-bottom:56.25%;height:auto;max-width:100%overflow:hidden"><iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
+    if markup =~ Syntax then
+      @id = $1
+
+      if $2.nil? then
+          @width = 560
+          @height = 420
       else
-        "Error processing input, expected syntax: {% youtube video_id [width height] %}"
+          @width = $2.to_i
+          @height = $3.to_i
       end
+    else
+      raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
-end
 
-Liquid::Template.register_tag('youtube', Jekyll::YouTubeTag)
+  def render(context)
+    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+  end
+
+  Liquid::Template.register_tag "youtube", self
+end
