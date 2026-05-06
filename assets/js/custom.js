82ef1096988fa6def03490cfb3e37286db6038b0
msj <msjo@devsight.kr>
2026-05-06 17:57:02 +0900

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.classList.add("loaded");
+document.addEventListener("readystatechange", () => {
+  if (document.readyState === "interactive") {
+    document.body.classList.add("loaded");
+  }
 });
-// 추가
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a");
+
+  links.forEach(link => {
+    if (
+      link.target === "_blank" ||
+      link.hasAttribute("download") ||
+      link.href.startsWith("javascript:") ||
+      link.href.includes("#")
+    ) {
+      return;
+    }
+
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const url = link.href;
+      document.body.classList.add("fade-out");
+      setTimeout(() => {
+        window.location.href = url;
+      }, 200); // CSS transition 시간과 맞춰야 함
+    });
+  });
+});
