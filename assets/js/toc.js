3ab416453b4a663db08b4348452716781f2c04df
msj <msjo@devsight.kr>
2026-05-11 00:51:17 +0900

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,8 +2,8 @@ document.querySelector("main h2, main h3")&&tocbot.refresh({
     tocSelector:"#toc",
     contentSelector:".content",
     ignoreSelector:"[data-toc-skip]",
-    headingSelector:"h2, h3, h4, h5",
+    headingSelector:"h2, h3, h4, h5, h6",
     orderedList:!1,
     scrollSmooth:!1,
-    collapseDepth: 5
+    collapseDepth: 6
 });
