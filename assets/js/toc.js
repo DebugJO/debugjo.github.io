72733ff7f76c5a2602a64381ad8785dbef42c059
msj <msjo@devsight.kr>
2026-05-11 21:21:45 +0900

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -7,8 +7,8 @@ document.querySelector("main h2, main h3, main h4, main h5") && tocbot.refresh({
     ignoreSelector: "[data-toc-skip]",
     headingSelector: "h2, h3, h4, h5",
     orderedList: !1,
-    scrollSmooth: !1, 
+    scrollSmooth: !0, 
     collapseDepth: 5,
     headingsOffset: dynamicOffset + 48, 
-    scrollSmoothOffset: -dynamicOffset
+    scrollSmoothOffset: -(dynamicOffset + 48)
 });
