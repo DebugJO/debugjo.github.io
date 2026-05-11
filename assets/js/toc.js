50adaab88b3c7c82ef3855883d1796d5f807e95b
msj <msjo@devsight.kr>
2026-05-11 20:48:25 +0900

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,3 +1,5 @@
+const dynamicOffset = document.querySelector('header').offsetHeight + 20;
+
 document.querySelector("main h2, main h3,  main h4,  main h5")&&tocbot.refresh({
     tocSelector:"#toc",
     contentSelector:".content",
@@ -5,5 +7,6 @@ document.querySelector("main h2, main h3,  main h4,  main h5")&&tocbot.refresh({
     headingSelector:"h2, h3, h4, h5",
     orderedList:!1,
     scrollSmooth:!1,
-    collapseDepth: 5
+    collapseDepth: 5,
+    headingsOffset: dynamicOffset
 });
