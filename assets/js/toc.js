2f8fe0240c79a445dbc39a0b4c2fcc8749971cf7
msj <msjo@devsight.kr>
2026-05-11 21:07:18 +0900

    Update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,12 +1,14 @@
-const dynamicOffset = document.querySelector('header').offsetHeight + 20;
+const headerElement = document.querySelector('header');
+const dynamicOffset = headerElement ? headerElement.offsetHeight : 0;
 
-document.querySelector("main h2, main h3,  main h4,  main h5")&&tocbot.refresh({
-    tocSelector:"#toc",
-    contentSelector:".content",
-    ignoreSelector:"[data-toc-skip]",
-    headingSelector:"h2, h3, h4, h5",
-    orderedList:!1,
-    scrollSmooth:!1,
+document.querySelector("main h2, main h3, main h4, main h5") && tocbot.refresh({
+    tocSelector: "#toc",
+    contentSelector: ".content",
+    ignoreSelector: "[data-toc-skip]",
+    headingSelector: "h2, h3, h4, h5",
+    orderedList: !1,
+    scrollSmooth: !1, 
     collapseDepth: 5,
-    headingsOffset: dynamicOffset
+    headingsOffset: dynamicOffset + 5, 
+    scrollSmoothOffset: -dynamicOffset
 });
