bd020616caae33639de1a0d4a83bd898c379a804
msj <msjo@devsight.kr>
2026-05-12 14:05:06 +0900

    Update toc-desktop.js

--- assets/js/toc-desktop.js
+++ assets/js/toc-desktop.js
@@ -1,14 +1,27 @@
-const headerElement = document.querySelector('header');
-const dynamicOffset = headerElement ? headerElement.offsetHeight : 0;
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 1200) {
+    return;
+  }
 
-document.querySelector("main h2, main h3, main h4, main h5") && tocbot.refresh({
-    tocSelector: "#toc",
-    contentSelector: ".content",
-    ignoreSelector: "[data-toc-skip]",
-    headingSelector: "h2, h3, h4, h5",
-    orderedList: !1,
-    scrollSmooth: !1, 
-    collapseDepth: 5,
-    headingsOffset: dynamicOffset, 
-    scrollSmoothOffset: -dynamicOffset
+  const headerElement =
+    document.querySelector('header');
+
+  const dynamicOffset = headerElement
+    ? headerElement.offsetHeight
+    : 0;
+
+  document.querySelector(
+    'main h2, main h3, main h4, main h5'
+  ) &&
+    tocbot.refresh({
+      tocSelector: '#toc',
+      contentSelector: '.content',
+      ignoreSelector: '[data-toc-skip]',
+      headingSelector: 'h2, h3, h4, h5',
+      orderedList: false,
+      scrollSmooth: false,
+      collapseDepth: 5,
+      headingsOffset: dynamicOffset,
+      scrollSmoothOffset: -dynamicOffset
+    });
 });
