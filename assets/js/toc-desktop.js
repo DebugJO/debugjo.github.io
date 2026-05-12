7ad0b24ea82890c33767224ed2830c2ed86b34d7
msj <msjo@devsight.kr>
2026-05-12 13:26:53 +0900

    Update toc-desktop.js

--- assets/js/toc-desktop.js
+++ assets/js/toc-desktop.js
@@ -15,15 +15,20 @@
 
 
 export class TocDesktop {
-  static options = {
-    tocSelector: '#toc',
-    contentSelector: '.content',
-    ignoreSelector: '[data-toc-skip]',
-    headingSelector: 'h2, h3, h4, h5',
-    orderedList: false,
-    scrollSmooth: false,
-    headingsOffset: 16 * 2 // 2rem
-  };
+  const headerElement = document.querySelector('header');
+  const dynamicOffset = headerElement ? headerElement.offsetHeight : 0;
+  
+  document.querySelector("main h2, main h3, main h4, main h5") && tocbot.refresh({
+      tocSelector: "#toc",
+      contentSelector: ".content",
+      ignoreSelector: "[data-toc-skip]",
+      headingSelector: "h2, h3, h4, h5",
+      orderedList: !1,
+      scrollSmooth: !1, 
+      collapseDepth: 5,
+      headingsOffset: dynamicOffset, 
+      scrollSmoothOffset: -dynamicOffset
+  });
 
   static refresh() {
     tocbot.refresh(this.options);
